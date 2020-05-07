54aa00979c4e1e037006e67a9c2177aadac7daff
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-05-07 13:08:24 -0500

    crear función enviar correo

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php
+//Llamando a los campos
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+//Datos para el correo
+
+$destinatario = "gustavo.soader.cx@gmail.com";
+$asunto = "Contacto de Nuestra Página Web";
+
+$carta = "De: $name \n";
+$carta .= "Correo: $email \n";
+$carta .= "Tema: $subject \n";
+$carta .= "Mensaje: $message \n";
+
+//Enviar mensaje
+mail($destinatario, $asunto, $carta);
+
+
+
+
+
+?>
