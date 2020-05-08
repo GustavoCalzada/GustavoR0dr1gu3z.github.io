6271d9c9ae7d04103500090b40c0ee41f95c30a1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-05-07 21:27:19 -0500

    commit correo2

--- enviar.php
+++ enviar.php
@@ -1,16 +1,18 @@
 <?php
 //Llamando a los campos
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
 
 //Datos para el correo
 
-$destinatario = "gustavo.soader.cx@gmail.com";
-$asunto = "Contacto de Nuestra Página Web";
+$destinatario="gustavo.soader.cx@gmail.com";
+$asunto="Contacto de Nuestra Pagina Web";
 
+$headers="MIME-Version: 1.0\r\n"; 
+$headers.=
 $carta = "De: $name \n";
 $carta .= "Correo: $email \n";
 $carta .= "Tema: $subject \n";
