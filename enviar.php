69b56d396ed9e24725c40da5f5d96aeb03f0dd09
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-05-22 18:26:39 -0500

    modificar archivo de correo

--- enviar.php
+++ enviar.php
@@ -1,23 +1,23 @@
 <?php
 //Llamando a los campos
 
-$name=$_POST['name'];
-$email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $message=$_POST['message'];
 
 //Datos para el correo
 
 $destinatario="gustavo.soader.cx@gmail.com";
 $asunto="Contacto de Nuestra Pagina Web";
 
-$headers="MIME-Version: 1.0\r\n"; 
-$headers.=
-$carta = "De: $name \n";
-$carta .= "Correo: $email \n";
-$carta .= "Tema: $subject \n";
-$carta .= "Mensaje: $message \n";
+
+$mensaje = "De: $name \n";
+$mensaje .= "Correo: $email \n";
+$mensaje .= "Tema: $subject \n";
+$mensaje .= "Mensaje: $message \n";
 
 //Enviar mensaje
-mail($destinatario, $asunto, $carta);
+mail($destinatario, $asunto, $mensaje);
+
 ?>
